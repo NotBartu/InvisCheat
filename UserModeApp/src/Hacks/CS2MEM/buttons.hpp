0f1d1039723278d14c2921541175049ba167cbd8
NotBartu <bomb.bartu@gmail.com>
2025-04-13 16:15:18 +0300

    Updated Offsets

--- UserModeApp/src/Hacks/CS2MEM/buttons.hpp
+++ UserModeApp/src/Hacks/CS2MEM/buttons.hpp
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2025-03-21 09:23:52.559600600 UTC
+// 2025-04-13 13:14:25.463363 UTC
 
 #pragma once
 
@@ -8,21 +8,21 @@
 namespace cs2_dumper {
     // Module: client.dll
     namespace buttons {
-        constexpr std::ptrdiff_t attack = 0x1885770;
-        constexpr std::ptrdiff_t attack2 = 0x1885800;
-        constexpr std::ptrdiff_t back = 0x1885A40;
-        constexpr std::ptrdiff_t duck = 0x1885D10;
-        constexpr std::ptrdiff_t forward = 0x18859B0;
-        constexpr std::ptrdiff_t jump = 0x1885C80;
-        constexpr std::ptrdiff_t left = 0x1885AD0;
-        constexpr std::ptrdiff_t lookatweapon = 0x1AAE3D0;
-        constexpr std::ptrdiff_t reload = 0x18856E0;
-        constexpr std::ptrdiff_t right = 0x1885B60;
-        constexpr std::ptrdiff_t showscores = 0x1AAE2B0;
-        constexpr std::ptrdiff_t sprint = 0x1885650;
-        constexpr std::ptrdiff_t turnleft = 0x1885890;
-        constexpr std::ptrdiff_t turnright = 0x1885920;
-        constexpr std::ptrdiff_t use = 0x1885BF0;
-        constexpr std::ptrdiff_t zoom = 0x1AAE340;
+        constexpr std::ptrdiff_t attack = 0x186C850;
+        constexpr std::ptrdiff_t attack2 = 0x186C8E0;
+        constexpr std::ptrdiff_t back = 0x186CB20;
+        constexpr std::ptrdiff_t duck = 0x186CDF0;
+        constexpr std::ptrdiff_t forward = 0x186CA90;
+        constexpr std::ptrdiff_t jump = 0x186CD60;
+        constexpr std::ptrdiff_t left = 0x186CBB0;
+        constexpr std::ptrdiff_t lookatweapon = 0x1A92F10;
+        constexpr std::ptrdiff_t reload = 0x186C7C0;
+        constexpr std::ptrdiff_t right = 0x186CC40;
+        constexpr std::ptrdiff_t showscores = 0x1A92DF0;
+        constexpr std::ptrdiff_t sprint = 0x186C730;
+        constexpr std::ptrdiff_t turnleft = 0x186C970;
+        constexpr std::ptrdiff_t turnright = 0x186CA00;
+        constexpr std::ptrdiff_t use = 0x186CCD0;
+        constexpr std::ptrdiff_t zoom = 0x1A92E80;
     }
 }
